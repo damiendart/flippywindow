3ff79ea85329e89d6df6fd78eea0fbe53e8cc5b7
Damien Dart <damiendart@pobox.com>
2018-03-15 20:11:26 +0000

    "BuildIcon.bat": Add copyright and licensing bits.

--- BuildIcon.bat
+++ BuildIcon.bat
@@ -1,3 +1,7 @@
+@REM Copyright (c) 2016-2018 Damien Dart, <https://www.robotinaponcho.net/>.
+@REM This file is distributed under the MIT licence. For more
+@REM information, please refer to the accompanying "LICENCE" file.
+
 magick convert FlippyWindow.svg -scale 16 tmp1.png
 magick convert FlippyWindow.svg -scale 32 tmp2.png
 magick convert FlippyWindow.svg -scale 48 tmp3.png
