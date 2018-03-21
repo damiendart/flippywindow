d32d5790dabeb1991909166719bb5e9ec23324e6
Damien Dart <damiendart@pobox.com>
2018-03-21 22:54:54 +0000

    Make "BuildIcon.bat" a little more robust.

--- BuildIcon.bat
+++ BuildIcon.bat
@@ -2,10 +2,21 @@
 @REM This file is distributed under the MIT licence. For more
 @REM information, please refer to the accompanying "LICENCE" file.
 
-magick convert FlippyWindow.svg -scale 16 tmp1.png
-magick convert FlippyWindow.svg -scale 32 tmp2.png
-magick convert FlippyWindow.svg -scale 48 tmp3.png
-magick convert FlippyWindow.svg -scale 128 tmp4.png
-magick convert FlippyWindow.svg -scale 256 tmp5.png
-magick convert tmp*.png FlippyWindow.ico
-del tmp*.png
+
+@ECHO OFF
+
+WHERE magick >nul 2>nul
+IF %ERRORLEVEL% EQU 0 (
+  magick convert FlippyWindow.svg -scale 16 tmp1.png
+  magick convert FlippyWindow.svg -scale 32 tmp2.png
+  magick convert FlippyWindow.svg -scale 48 tmp3.png
+  magick convert FlippyWindow.svg -scale 128 tmp4.png
+  magick convert FlippyWindow.svg -scale 256 tmp5.png
+  magick convert tmp*.png FlippyWindow.ico
+  del tmp*.png
+  ECHO "FlippyWindow.ico" created!
+) ELSE (
+  ECHO ERROR: ImageMagick is required.
+)
+
+@ECHO ON
